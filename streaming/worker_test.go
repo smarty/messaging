9f401252ed46ab88296ef552c6637dc5c57d1151
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-04-01 21:05:48 -0600

    Tweaked tests.

--- streaming/worker_test.go
+++ streaming/worker_test.go
@@ -143,9 +143,9 @@ func (this *WorkerFixture) TestWhenStreamingToHandler_BatchOfMessagesPushedToHan
 	this.So(this.acknowledgeCount, should.Equal, 1)
 	this.So(this.acknowledgeDeliveries, should.Resemble, deliveries)
 }
-func (this *WorkerFixture) TestWhenStreamingToHandler_DoNotPushDeliveriesWithBlankMessageToHandlers() {
+func (this *WorkerFixture) TestWhenStreamingToHandler_DoNotPushDeliveriesWithBlankMessage_ButStillAckAllDeliveries() {
 	this.readError = io.EOF
-	deliveries := []messaging.Delivery{{Message: 1}, {Message: 2}, { /* delivery nil/blank message */ }}
+	deliveries := []messaging.Delivery{{Message: 1}, { /* delivery nil/blank message */ }, {Message: 3}, { /* delivery nil/blank message */ }}
 	for _, delivery := range deliveries {
 		this.channelBuffer <- delivery
 	}
@@ -154,13 +154,12 @@ func (this *WorkerFixture) TestWhenStreamingToHandler_DoNotPushDeliveriesWithBla
 
 	this.So(this.handleCtx, should.Equal, this.hardContext)
 	this.So(this.handleCount, should.Equal, 1)
-	this.So(this.handleMessages, should.Resemble, []interface{}{1, 2})
+	this.So(this.handleMessages, should.Resemble, []interface{}{1, 3})
 
 	this.So(this.acknowledgeContext, should.Equal, this.hardContext)
 	this.So(this.acknowledgeCount, should.Equal, 1)
 	this.So(this.acknowledgeDeliveries, should.Resemble, deliveries)
 }
-
 func (this *WorkerFixture) TestWhenMoreDeliveriesExistThanBatchMax_DeliverInBatchesOfMaxSpecifiedSize() {
 	this.subscription.batchCapacity = 2
 	this.subscription.bufferCapacity = 5
