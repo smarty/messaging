6b3d12779d93110a3cb330072f87538e4913dc06
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2022-06-16 15:16:58 -0600

    Whitespace.

--- sqlmq/_schema_mysql.sql
+++ sqlmq/_schema_mysql.sql
@@ -1,9 +1,8 @@
 CREATE TABLE Messages (
-	id bigint unsigned AUTO_INCREMENT NOT NULL,
-	dispatched datetime(3) NULL,
-	type varchar(256) NOT NULL,
-	payload mediumblob NOT NULL,
-	PRIMARY KEY (id)
+    id         bigint unsigned AUTO_INCREMENT NOT NULL,
+    dispatched datetime(3)                        NULL,
+    type       varchar(256)                   NOT NULL,
+    payload    mediumblob                     NOT NULL,
+    PRIMARY KEY (id)
 );
-
 CREATE UNIQUE INDEX ix_messages_dispatched ON Messages (dispatched, id);
