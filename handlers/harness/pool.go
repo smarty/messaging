7c4f36db3d7ccd44535d991cfc135c6df6c2142a
Michael Whatcott <mike@smarty.com>
2026-06-01 11:17:02 -0600

    Consistent receiver.

--- handlers/harness/pool.go
+++ handlers/harness/pool.go
@@ -9,5 +9,5 @@ type poolT[T any] struct{ pool *sync.Pool }
 func newPoolT[T any](new func() T) *poolT[T] {
 	return &poolT[T]{pool: &sync.Pool{New: func() any { return new() }}}
 }
-func (p *poolT[T]) Get() T  { return p.pool.Get().(T) }
-func (p *poolT[T]) Put(t T) { p.pool.Put(t) }
+func (this *poolT[T]) Get() T  { return this.pool.Get().(T) }
+func (this *poolT[T]) Put(t T) { this.pool.Put(t) }
