e5f18641f6d27fd7f759a08a727872ea200b5831
Michael Whatcott <mike@smarty.com>
2026-06-05 15:38:22 -0600

    Remove unused fields.

--- handlers/harness/message.go
+++ handlers/harness/message.go
@@ -1,20 +1,17 @@
 package harness
 
-import (
-	"bytes"
-	"time"
-)
+import "bytes"
 
 type Message struct {
 	// ID represents the unique ID of this message and its sequential place within a larger stream.
 	ID uint64
 
-	// Value contains the in-memory Go message structure.
-	Value any
-
 	// Type is the name registered for the (Go) type of the Value.
 	// (i.e. 'subscription:subscription-renewed-v2').
 	Type string
+
+	// Value contains the in-memory Go message structure.
+	Value any
 
 	// Content contains the serialized representation of the Go Value.
 	Content *bytes.Buffer
@@ -22,16 +19,4 @@ type Message struct {
 	// ContentType identifies the serialization method employed to represent the Content
 	// (i.e. 'application/json; charset=utf-8').
 	ContentType string
-
-	// ContentEncoding identifies the encoding of the Content
-	// (i.e. 'gzip').
-	ContentEncoding string
-
-	// Stored represents the instant the Type, Content, ContentType, and ContentEncoding
-	// were durably stored (a critically important step in processing--if it's not stored, it didn't happen).
-	Stored time.Time
-
-	// Dispatched represents the instant the Type, Content, ContentType, and ContentEncoding
-	// were published to the external messaging system (if configured).
-	Dispatched time.Time
 }
